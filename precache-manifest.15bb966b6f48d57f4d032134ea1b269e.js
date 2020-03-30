0cd880757f7f25c89b6869e570b236b0f9589432
Gonzalo-GC <gonzalo_gallego@outlook.com>
2020-03-30 18:49:44 +0200

    Updates

---
+++ precache-manifest.15bb966b6f48d57f4d032134ea1b269e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6549527becbe2bfdcaef792fc480399",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f6c582f89625b5f1d17d",
+    "url": "/portfolio/static/css/main.31339452.chunk.css"
+  },
+  {
+    "revision": "b96f08714e4a2ea69fe3",
+    "url": "/portfolio/static/js/2.651b7270.chunk.js"
+  },
+  {
+    "revision": "90fb4deb912422b947a6490378160cce",
+    "url": "/portfolio/static/js/2.651b7270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c582f89625b5f1d17d",
+    "url": "/portfolio/static/js/main.b4408efb.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "5e2771098ee7c6855a1fb1d3bb4f73d0",
+    "url": "/portfolio/static/media/about_image.5e277109.jpg"
+  },
+  {
+    "revision": "cd54b4f7edbc8ef3fe01ea6fcc162730",
+    "url": "/portfolio/static/media/home_2.cd54b4f7.jpg"
+  }
+]);
